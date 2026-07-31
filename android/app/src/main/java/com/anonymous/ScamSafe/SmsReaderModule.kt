e6989ac28026d25e2dc5521625af5a2c7a429ae0
Prem36Kumar <premkumarqu@gmail.com>
2026-07-31 11:39:38 +0000

    fix: add SmsReaderModule + register in MainApplication

---
+++ android/app/src/main/java/com/anonymous/ScamSafe/SmsReaderModule.kt
@@ -0,0 +1,41 @@
+package com.anonymous.ScamSafe
+
+import android.net.Uri
+import com.facebook.react.bridge.*
+
+class SmsReaderModule(private val ctx: ReactApplicationContext) : ReactContextBaseJavaModule(ctx) {
+  override fun getName() = "SmsReaderModule"
+
+  @ReactMethod
+  fun getSms(sinceDays: Int, promise: Promise) {
+    try {
+      val since = System.currentTimeMillis() - sinceDays * 24L * 60 * 60 * 1000
+      val list  = mutableListOf<WritableMap>()
+      val cursor = ctx.contentResolver.query(
+        Uri.parse("content://sms/inbox"),
+        arrayOf("_id", "address", "body", "date"),
+        "date > ?", arrayOf(since.toString()), "date DESC"
+      )
+      cursor?.use {
+        val iId   = it.getColumnIndex("_id")
+        val iAddr = it.getColumnIndex("address")
+        val iBody = it.getColumnIndex("body")
+        val iDate = it.getColumnIndex("date")
+        var count = 0
+        while (it.moveToNext() && count < 200) {
+          val m = Arguments.createMap()
+          m.putString("_id",     it.getString(iId)   ?: "")
+          m.putString("address", it.getString(iAddr) ?: "")
+          m.putString("body",    it.getString(iBody) ?: "")
+          m.putDouble("date",    it.getString(iDate)?.toDoubleOrNull() ?: 0.0)
+          list.add(m); count++
+        }
+      }
+      val arr = Arguments.createArray()
+      list.forEach { arr.pushMap(it) }
+      promise.resolve(arr)
+    } catch (e: Exception) {
+      promise.reject("SMS_ERROR", e.message)
+    }
+  }
+}
---
+++ android/app/src/main/java/com/anonymous/ScamSafe/SmsReaderPackage.kt
@@ -0,0 +1,8 @@
+package com.anonymous.ScamSafe
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.*
+import com.facebook.react.uimanager.ViewManager
+class SmsReaderPackage : ReactPackage {
+  override fun createNativeModules(ctx: ReactApplicationContext) = listOf(SmsReaderModule(ctx))
+  override fun createViewManagers(ctx: ReactApplicationContext): List<ViewManager<*,*>> = emptyList()
+}
